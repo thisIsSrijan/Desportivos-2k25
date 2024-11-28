3baef02fbe5ff7c36e773903d03a829730a71df0
vanshika-thadani <vanshikathadani623@gmail.com>
2024-11-29 00:33:44 +0530

    responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
+import "./app.css"; 
 
 function App() {
   return (
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       fontFamily: {
         dharma: ['"Dharma Gothic C"', 'sans-serif'], 
       },
+      screens: {
+        "xsm": "200px",
+        "sm": "576px", // Small devices (phones, ≥ 576px)
+        "md": "768px", // Medium devices (tablets, ≥ 768px)
+        "lg": "992px", // Large devices (desktops, ≥ 992px)
+        "xl": "1200px", // Extra large devices (large desktops, ≥ 1200px)
+        "2xl": "1400px", // Extra extra large devices (larger desktops, ≥ 1400px)
+        'customBreakpoint': '400px',
+        "uxl": "1500px"
+  
+      },
     },
   },
   plugins: [],
