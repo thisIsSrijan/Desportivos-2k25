ea9d7bd107c10449363c1c669323f3cf0e7a1bbf
Srijan Das <dassrijan16@gmail.com>
2024-12-13 21:27:57 +0530

    minor changes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -153,7 +153,7 @@ const Register = () => {
       </div>
       <Link
         to="/"
-        className="text-center align-middle absolute w-fit h-fit py-2 px-5 right-8 sm:right-16 sm:top-[75px] top-[64px] border-2 border-[#F85B02] shadow-[2px_1.5px_13px_#F85B02] rounded-xl z-10 text-white text-xl lg:text-3xl bg-[#424242BF] font-squid hover:bg-[#515151bf] hover:scale-105"
+        className="text-center align-middle absolute w-fit h-fit py-2 px-5 right-[16px] sm:right-16 sm:top-[75px] top-[26px] border-2 border-[#F85B02] shadow-[2px_1.5px_13px_#F85B02] rounded-xl z-10 text-white text-l sm:text-xl lg:text-3xl bg-[#424242BF] font-squid hover:bg-[#515151bf] hover:scale-105"
       >
         HOME
       </Link>
@@ -258,11 +258,12 @@ const Register = () => {
               >
                 <option value="">Select a City</option>
                 {cityOptions.map((option) => (
-                  <option key={option.isoCode} 
-                  value={option.name}
-                  className={`text-black bg-[#493d33] hover:bg-[#6b5b4c] ${
-                    state === option.isoCode ? "bg-orange-600 text-white" : ""
-                  }`}
+                  <option
+                    key={option.isoCode}
+                    value={option.name}
+                    className={`text-black bg-[#493d33] hover:bg-[#6b5b4c] ${
+                      state === option.isoCode ? "bg-orange-600 text-white" : ""
+                    }`}
                   >
                     {option.name}
                   </option>
@@ -328,16 +329,16 @@ const Register = () => {
             <button
               type="submit"
               onClick={handleSubmit}
-              className="min-w-36 bg-transparent text-[rgba(164,164,164,1)] font-extrabold leading-[60px] tracking-[5%] border px-2  m-2 hover:bg-black"
+              className="min-w-36 bg-transparent text-xl text-[rgba(164,164,164,1)] font-extrabold leading-[60px] tracking-[5%] border px-2  m-2  hover:text-[#F6931C] hover:border-[#F6931C] hover:duration-300 hover:bg-[#1f1f1f]"
             >
               REGISTER
             </button>
 
             <button
-              className="min-w-36 bg-transparent text-[rgba(164,164,164,1)] font-extrabold leading-[60px] tracking-[5%] border px-2 m-2 hover:bg-black"
+              className="min-w-36 bg-transparent text-xl text-[rgba(164,164,164,1)] font-extrabold leading-[60px] tracking-[5%] border px-2 m-2 hover:text-[#F6931C] hover:border-[#F6931C] hover:duration-300 hover:bg-[#1f1f1f]"
               onClick={handleRuleBook}
             >
-              RULEBOOK
+              GUIDELINES
             </button>
           </div>
         </div>
