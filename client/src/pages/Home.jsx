de8798f2b328aab1a56473039fc1ecd32f597a88
vanshika-thadani <vanshikathadani623@gmail.com>
2024-12-22 18:37:30 +0530

    Resolved merge conflict in Home.jsx and added updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import AboutUs from "./AboutUs";
+import Sports from "./Sports";
 
 const Home = () => {
   return (
     <div>
       <Carousel />
       <AboutUs />
+      <Sports/>
     </div>
   );
 };
